a4984dbaadbf67feef00eda2aa0b97e798a7084d
Anik Islam <aanikbsmrstucse@gmail.com>
2023-06-10 21:05:12 +0600

    daisy ui fixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is home pages</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is home pages</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
 };
 
 export default Home;
