28ad1b3e0ba8dcf3bd72733edbd12f7ff3030005
Anik Islam <aanikbsmrstucse@gmail.com>
2023-06-11 12:38:56 +0600

    small nav bar done

--- src/Pages/SharedPages/Navbar/Navbar.jsx
+++ src/Pages/SharedPages/Navbar/Navbar.jsx
@@ -1,12 +1,46 @@
 import React from "react";
-
+import {
+    FaEnvelope,
+    FaFacebook,
+    FaLinkedin,
+    FaPhone,
+    FaTwitter,
+    FaYoutube,
+} from "react-icons/fa";
 const Navbar = () => {
-    const menuBar = <>
-    <li>Home</li>
+  const menuBar = (
+    <>
+      <li>Home</li>
     </>
+  );
   return (
     <div>
-      <div></div>
+      <div className="md:h-[40px] h-[70px] w-full md:gap-0 gap-1 bg-[#3b3b3b] flex md:flex-row flex-col items-center px-5 flex-wrap md:pt-0 pt-2">
+        <div className="text-[#acaaaa] flex md:gap-3 gap-1 flex-row md:flex-grow md:mt-0 mt-5">
+          <FaFacebook />
+          <FaYoutube /> <FaLinkedin /> <FaTwitter />
+        </div>
+        <div className="text-[#acaaaa] flex md:gap-3 flex-col md:flex-row items-center text-sm md:mr-7">
+          {" "}
+          <div className="flex md:gap-2 items-center">
+            <FaPhone></FaPhone>+228 872 4444
+          </div>{" "}
+          <div className="flex gap-2 items-center">
+            <FaPhone />
+            +775 872 4444
+          </div>{" "}
+          <div className="flex gap-2 items-center">
+            <FaEnvelope />
+            contact@email.com
+          </div>
+        </div>
+        <div>
+          <select className="select select-bordered-0 rounded-none border-0   select-sm w-[90px] bg-[#3b3b3b] max-w-xs text-[#acaaaa] md:mt-0 mt-4">
+            <option selected className="text-sm">White</option>
+            <option className="text-sm">Dark</option>
+          </select>
+        </div>
+      </div>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -29,19 +63,15 @@ const Navbar = () => {
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              
-            </ul>
+            ></ul>
           </div>
           <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-           
-          </ul>
+          <ul className="menu menu-horizontal px-1"></ul>
         </div>
         <div className="navbar-end">
-          <a className="btn btn-outline normal-case">login</a>
+          <a className="btn btn-outline normal-case btn-sm">Login</a>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./fonts.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="max-w-7xl mx-auto">
+  <div className="max-w-7xl mx-auto ">
     <React.StrictMode>
       <HelmetProvider>
         <RouterProvider router={router}></RouterProvider>
