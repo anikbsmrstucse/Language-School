4e85794b88b788c52c939bd84f9b7000eef6869d
Anik Islam <aanikbsmrstucse@gmail.com>
2023-06-12 12:25:32 +0600

    google user fucntionality done

--- src/Pages/SocialSignIn/SocialSignIn.jsx
+++ src/Pages/SocialSignIn/SocialSignIn.jsx
@@ -1,19 +1,52 @@
 import React, { useContext } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const SocialSignIn = () => {
-    const {googleSignIn} = useContext(AuthContext);
+  const { googleSignIn } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
 
-    const handleGoogle = () => {
-      googleSignIn()
-      .then(result => {
+  const handleGoogle = () => {
+    googleSignIn()
+      .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
+        const saveInfo = {
+          name: loggedUser.name,
+          emaii: loggedUser.email,
+        };
+        fetch("http://localhost:5000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(saveInfo),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.insertedId) {
+              Swal.fire({
+                title: "Register Successfully",
+                icon: "success",
+                showClass: {
+                  popup: "animate__animated animate__fadeInDown",
+                },
+                hideClass: {
+                  popup: "animate__animated animate__fadeOutUp",
+                },
+              });
+              navigate(from,{replace:true});
+            }
+          });
       })
-      .catch(error =>{
+      .catch((error) => {
         console.log(error);
-      })
-    }
+      });
+  };
   return (
     <div>
       <div className="divider w-11/12 mx-auto">OR</div>
